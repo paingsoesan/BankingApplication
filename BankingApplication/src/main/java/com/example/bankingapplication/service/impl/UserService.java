e6d94e93ec937147ff928ba345f74b01b4029374
paingsoesan <113500260+paingsoesan@users.noreply.github.com>
2024-07-01 02:59:18 +0700

    Delete BankingApplication directory

--- BankingApplication/src/main/java/com/example/bankingapplication/service/impl/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankingapplication.service.impl;
-
-import com.example.bankingapplication.dto.BankResponse;
-import com.example.bankingapplication.dto.UserRequest;
-
-public interface UserService {
-
-    BankResponse createAccount(UserRequest userRequest);
-}
